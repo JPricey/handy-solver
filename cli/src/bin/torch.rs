2e63ff6248a6e129e85965223b49b6705298f18e
Joe Price <jc2price@gmail.com>
2024-01-06 17:02:06 -0800

    starter heuristic done

--- cli/src/bin/torch.rs
+++ cli/src/bin/torch.rs
@@ -18,9 +18,56 @@ const PILE_SIZE: i64 = 9;
 const CARD_SIZE: i64 = 13;
 const INPUT_SIZE: i64 = PILE_SIZE * CARD_SIZE;
 
+fn basic_heuristic(pile: &Pile) -> f32 {
+    let mut score: f32 = 0.0;
+    let mut consecutive_monster_mod = 1.0;
+
+    for card_ptr in pile.iter() {
+        let active_face = card_ptr.get_active_face();
+        // Vampire master special case
+        if card_ptr.card_id == 18 {
+            let weight_per_side = 6.0;
+            score += match card_ptr.key {
+                FaceKey::A => weight_per_side * 3.0,
+                FaceKey::B => weight_per_side * 2.0,
+                FaceKey::D => weight_per_side * 1.0,
+                FaceKey::C => 0.0,
+            };
+        } else {
+            score += match active_face.allegiance {
+                Allegiance::Baddie => {
+                    let health_score = match active_face.health {
+                        Health::Empty => 0.0,
+                        Health::Half => 1.0,
+                        Health::Full => 2.0,
+                    };
+
+                    health_score + consecutive_monster_mod
+                }
+                Allegiance::Hero => {
+                    consecutive_monster_mod = 0.0;
+
+                    match active_face.health {
+                        Health::Empty => -0.2,
+                        Health::Half => -0.1,
+                        Health::Full => 0.0,
+                    }
+                }
+                Allegiance::Werewolf => {
+                    consecutive_monster_mod = 0.0;
+
+                    0.0
+                }
+            }
+        }
+    }
+
+    return score;
+}
+
 fn net(vs: &nn::Path) -> impl Module {
     let conv_output = 16;
-    let l1_output = 64;
+    let l1_output = 128;
     let l2_output = 64;
 
     nn::seq()
@@ -56,7 +103,6 @@ fn net(vs: &nn::Path) -> impl Module {
         ))
         .add_fn(|xs| xs.relu())
         .add(nn::linear(vs / "layer3", l2_output, 1, Default::default()))
-        .add_fn(|xs| xs.sigmoid())
 }
 
 fn to_byte_slice<'a>(floats: &'a [f32]) -> &'a [u8] {
@@ -105,6 +151,12 @@ fn _init_card_map(card_map: &mut CardMap, pile: &Pile) {
     }
 }
 
+pub fn get_fully_random_pile<R: Rng>(hero: Class, monster: Class, rng: &mut R) -> Pile {
+    let mut pile = get_start_from_classes(hero, monster, rng);
+    randomize_sides(&mut pile, rng);
+    return pile;
+}
+
 pub fn get_random_pile_with_no_winner<R: Rng>(hero: Class, monster: Class, rng: &mut R) -> Pile {
     let mut pile = get_start_from_classes(hero, monster, rng);
     randomize_sides(&mut pile, rng);
@@ -131,11 +183,47 @@ pub fn randomize_sides<R: Rng>(pile: &mut Pile, rng: &mut R) {
     }
 }
 
+fn generate_fully_random_basic_heuristic_example<R: Rng>(
+    card_map: &CardMap,
+    hero: Class,
+    monster: Class,
+    rng: &mut R,
+) -> (Tensor, Tensor) {
+    let pile = get_fully_random_pile(hero, monster, rng);
+    let x = pile_onehot(card_map, &pile);
+    let pile_score = basic_heuristic(&pile);
+    let y = Tensor::from_slice(&[pile_score]);
+
+    (x, y)
+}
+
+fn generate_basic_example_batch<R: Rng>(
+    batch_size: usize,
+    card_map: &CardMap,
+    hero: Class,
+    monster: Class,
+    rng: &mut R,
+) -> (Tensor, Tensor) {
+    let mut v_xs = Vec::with_capacity(batch_size);
+    let mut v_ys = Vec::with_capacity(batch_size);
+
+    for _ in 0..batch_size {
+        let (x, y) = generate_fully_random_basic_heuristic_example(card_map, hero, monster, rng);
+        v_xs.push(x);
+        v_ys.push(y);
+    }
+
+    (Tensor::stack(&v_xs, 0), Tensor::stack(&v_ys, 0))
+}
+
 fn main() {
     let hero = Class::Cursed;
     let monster = Class::Spider;
     let mut rng = thread_rng();
     let mut card_map: CardMap = HashMap::new();
+    let test_batch_size = 25;
+    let runs_per_test = 5000;
+    let train_batch_size = 10;
 
     {
         let pile = get_start_from_classes(hero, monster, &mut rng);
@@ -148,49 +236,50 @@ fn main() {
     dbg!(&net);
 
     loop {
-        for batch in 1..1000 {
-            {
-                let pile = get_random_pile_with_no_winner(hero, monster, &mut rng);
-                let xs = pile_onehot(&card_map, &pile).unsqueeze(0);
-                let ys = Tensor::from_slice(&[1.0]).unsqueeze(0);
-                let loss = (net.forward(&xs) - ys)
-                    .pow_tensor_scalar(2)
-                    .sum(Kind::Float);
-                opt.backward_step(&loss);
+        {
+            let (xs, ys) =
+                generate_basic_example_batch(test_batch_size, &card_map, hero, monster, &mut rng);
+            let res = net.forward(&xs);
+            let diffs = &res - &ys;
+            let loss = diffs.pow_tensor_scalar(2).sum(Kind::Float);
+
+            // let v_diffs = Vec::with_capacity(test_batch_size);
+            for i in 0..test_batch_size {
+                let exp = ys.double_value(&[i as i64, 0]);
+                let actual = res.double_value(&[i as i64, 0]);
+                println!("{:.2} {:.2} / {:.2}", exp, actual, exp - actual);
             }
 
-            {
-                let pile = get_random_won_pile(hero, monster, &mut rng);
-                let xs = pile_onehot(&card_map, &pile).unsqueeze(0);
-                let ys = Tensor::from_slice(&[0.0]).unsqueeze(0);
-                let loss = (net.forward(&xs) - ys)
-                    .pow_tensor_scalar(2)
-                    .sum(Kind::Float);
-                opt.backward_step(&loss);
-            }
+            dbg!(loss);
         }
 
-        {
-            {
-                let pile = get_random_pile_with_no_winner(hero, monster, &mut rng);
-                let xs = pile_onehot(&card_map, &pile).unsqueeze(0);
-                let ys = Tensor::from_slice(&[1.0]).unsqueeze(0);
-                let non_res = net.forward(&xs);
-                let non_acc = non_res.accuracy_for_logits(&ys);
-                dbg!((non_res.f_double_value(&[0, 0]), non_acc,));
+        for _ in 1..runs_per_test {
+            let (xs, ys) =
+                generate_basic_example_batch(train_batch_size, &card_map, hero, monster, &mut rng);
+            let loss = (net.forward(&xs) - ys)
+                .pow_tensor_scalar(2)
+                .sum(Kind::Float);
+            opt.backward_step(&loss);
 
-                let pile = get_random_won_pile(hero, monster, &mut rng);
-                let xs = pile_onehot(&card_map, &pile).unsqueeze(0);
-                let ys = Tensor::from_slice(&[0.0]).unsqueeze(0);
-                let won_res = net.forward(&xs);
-                let won_acc = won_res.accuracy_for_logits(&ys);
+            // {
+            //     let pile = get_random_pile_with_no_winner(hero, monster, &mut rng);
+            //     let xs = pile_onehot(&card_map, &pile).unsqueeze(0);
+            //     let ys = Tensor::from_slice(&[1.0]).unsqueeze(0);
+            //     let loss = (net.forward(&xs) - ys)
+            //         .pow_tensor_scalar(2)
+            //         .sum(Kind::Float);
+            //     opt.backward_step(&loss);
+            // }
 
-                dbg!((won_res.f_double_value(&[0, 0]), won_acc));
-                // println!(
-                //     "test acc: {:5.2}%",
-                //     100. * f64::from(&test_accuracy),
-                // );
-            }
+            // {
+            //     let pile = get_random_won_pile(hero, monster, &mut rng);
+            //     let xs = pile_onehot(&card_map, &pile).unsqueeze(0);
+            //     let ys = Tensor::from_slice(&[0.0]).unsqueeze(0);
+            //     let loss = (net.forward(&xs) - ys)
+            //         .pow_tensor_scalar(2)
+            //         .sum(Kind::Float);
+            //     opt.backward_step(&loss);
+            // }
         }
     }
 
