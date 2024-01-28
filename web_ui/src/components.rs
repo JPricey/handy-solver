372a412e586095bed22042ee879671631a13f407
Joe Price <jc2price@gmail.com>
2024-01-28 12:17:29 -0800

    Add icon badge

--- web_ui/src/components.rs
+++ web_ui/src/components.rs
@@ -6,6 +6,7 @@ pub mod pile_span;
 pub mod utils;
 pub mod index_badge;
 pub mod token_span;
+pub mod icon_badge;
 
 pub use button::*;
 pub use card_id_pill::*;
@@ -15,3 +16,4 @@ pub use pile_span::*;
 pub use utils::*;
 pub use index_badge::*;
 pub use token_span::*;
+pub use icon_badge::*;
